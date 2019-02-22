d90b89cd29ed43d7d51e678c4997b5e56bf6d507
Steven G. Harms <steven.harms@flatironschool.com>
2019-02-22 16:58:19 -0500

    Update test framework

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function fetchBooks() {
 }
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
