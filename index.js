ea6ef9eb13a8fc5f8953a4ff6f782c3786463e9f
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2019-05-14 12:01:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
     .then(resp => resp.json())
     .then(json => renderBooks(json))
 }
