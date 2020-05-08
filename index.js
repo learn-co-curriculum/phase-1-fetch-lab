d0cdbe983e76b128cb13fa25c517b96d0d6baf90
Maxwell Benton <maxwellbenton@gmail.com>
2020-05-08 17:41:18 -0400

    update starter code

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function fetchBooks() {
 
 }
 
-function renderBooks(json) {
+function renderBooks(books) {
   const main = document.querySelector('main')
-  json.forEach(book => {
+  books.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
+    h2.innerHTML = book.name
     main.appendChild(h2)
   })
 }
