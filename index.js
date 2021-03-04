8d002457ecd1b52e44c0a1778782e17acc26c964
Liz Burton <liz.burton147@gmail.com>
2021-03-04 07:23:54 -0500

    edits for consistency

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function fetchBooks() {
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+  fetchBooks();
+});
