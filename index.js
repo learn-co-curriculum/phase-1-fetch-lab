7cf7bf3407f7fdadabc25bc1dcdab29df19e9cf0
Hpollard003 <hpollard003@gmail.com>
2021-04-14 02:15:53 -0400

    commit tree 21eacc111659ba6df035280ef55361d19cca676c

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  //.then(json => console.log(json));
+  .then(json => renderBooks(json))
   
 }
 
