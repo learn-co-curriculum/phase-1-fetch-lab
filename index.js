29ba0c3de2f25e16e6b5a6db6a28128a526f5564
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-05 12:26:07 -0600

    commit tree 6ccccdc310eab8175ff8ef3d365a1ba422efd142

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+// function fetchBooks() {
+//   fetch("https://anapioficeandfire.com/api/books")
+//   .then(res => res.json())
+//   .then(x => renderBooks(x))
+// }
+
+// function renderBooks(books) {
+//   const main = document.querySelector("main")
+//   books.forEach(book => {
+//     const h2 = document.createElement("h2");
+//     h2.innerHTML = book.name
+//     main.appendChild('h2')
+//   })
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   fetchBooks()
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ function fetchBooks() {
+   fetch("https://anapioficeandfire.com/api/books")
+   .then((res) => res.json())
+   .then(json => renderBooks(json))
+ }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+   const main = document.querySelector('main');
+   books.forEach(book => {
+     const h2 = document.createElement('h2');
+     h2.innerHTML = book.name;
+     main.appendChild(h2);
+   });
+ }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+ document.addEventListener('DOMContentLoaded', function() {
+   fetchBooks();
 });
